8c137d4ea789a8c0233d15fa7ac6e48adc3fe264
Alan Coomes <acoomes33@gmail.com>
2022-07-18 02:08:47 -0600

    added fetch and display functions

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //eventhandler for fetching advice api data on load or triggered by button
 
-fetch ("https://api.adviceslip.com/advice")
+const fetchAnimals = () => {
+fetch("https://api.adviceslip.com/advice")
 .then(res => res.json())
 .then(data => displayAdvice(data))
-
-
+.catch(err => alert("Something went wrong"))
+}
+function displayAdvice(data) {
+    let adviceText = document.getElementById("advice-text")
+    let adviceId = document.getElementById("advice-id")
+    adviceText.innerText = `"${data.advice}"`
+    adviceId.innerText = `Advice #${data.slip_id}`
+}
